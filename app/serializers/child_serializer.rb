64cdfd9fe20b908446c21eadd0f7194485c4364c
Amy Posten <amy@amyposten.io>
2021-12-23 10:51:34 -0500

    Active storage

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,6 +1,6 @@
 class ChildSerializer < ActiveModel::Serializer 
-  attributes :id, :first_name, :age, :image
   include Rails.application.routes.url_helpers
+  attributes :id, :first_name, :age, :image
 
   def image
   #   object.image.attached?  url: rails_blob_url(object.image)
